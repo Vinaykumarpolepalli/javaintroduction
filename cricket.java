2f8abbeba7f7a1ea7f6f938f8fa2fbffe60783f7
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:30:44 +0530

    Add files via upload

---
+++ cricket.java
@@ -0,0 +1,159 @@
+package javademo1;
+
+public class cricket {
+	//Instance
+	int jerscyidnumber;
+	String playername;
+	public int jno;
+	//static
+	static int countrynumber=91;
+	static String countryname="india";
+	
+	
+
+	public static void main(String[] args) {
+		System.out.println("welcome to india players");
+		System.out.println("counterynumber : "+ countrynumber);
+		System.out.println("counteryname : "+ countryname);
+		
+		
+		
+		
+		System.out.println("all the best for team");
+		cricket c1 = new cricket();
+		//countrynumber =92;
+		//countryname ="bharat";
+		c1.jerscyidnumber=18;
+		c1.playername    ="virat kohal";
+		System.out.println("playernumber:1");
+		System.out.println("countrynumber : "+ countrynumber);
+		System.out.println("countryname : "+ countryname);
+		System.out.println("jerscyidnumber : "+ c1.jerscyidnumber);
+		System.out.println("playername : "+ c1.playername);
+		
+		
+		
+		
+		
+		cricket c2 = new cricket();
+		c2.jerscyidnumber=07;
+		c2.playername    ="msdhone";
+		System.out.println("playernumber:2");
+		System.out.println("countrynumber : "+ countrynumber);
+		System.out.println("countryname : "+ countryname);
+		System.out.println("jerscyidnumber : "+ c2.jerscyidnumber);
+		System.out.println("playername : "+ c2.playername);
+	
+		
+		cricket c3 = new cricket();
+		c3.jerscyidnumber=45;
+		c3.playername    ="rohit shrama";
+		System.out.println("playernumber:3");
+		System.out.println("countrynumber : "+ countrynumber);
+		System.out.println("countryname : "+ countryname);
+		System.out.println("jerscyidnumber : "+ c3.jerscyidnumber);
+		System.out.println("playername : "+ c3.playername);
+		
+		
+		cricket c4 = new cricket();
+		c4.jerscyidnumber=33;
+		c4.playername    ="hardika pandya";
+		System.out.println("playernumber:4");
+		System.out.println("countrynumber : "+ countrynumber);
+		System.out.println("countryname : "+ countryname);
+		System.out.println("jerscyidnumber : "+ c4.jerscyidnumber);
+		System.out.println("playername : "+ c4.playername);
+		
+		
+		cricket c5= new cricket();
+		c5.jerscyidnumber=48;
+		c5.playername    ="suresh rani";
+		System.out.println("playernumber:5");
+		System.out.println("countrynumber : "+ countrynumber);
+		System.out.println("countryname : "+ countryname);
+		System.out.println("jerscyidnumber : "+ c5.jerscyidnumber);
+		System.out.println("playername : "+ c5.playername);
+		
+		
+		cricket c6 = new cricket();
+		c6.jerscyidnumber=28;
+		c6.playername    ="rahul chahar";
+		System.out.println("playernumber:6");
+		System.out.println("countrynumber : "+ countrynumber);
+		System.out.println("countryname : "+ countryname);
+		System.out.println("jerscyidnumber : "+ c6.jerscyidnumber);
+		System.out.println("playername : "+ c6.playername);
+		
+		
+		
+		cricket c7 = new cricket();
+		c7.jerscyidnumber=01;
+		c7.playername    ="klrahul";
+		System.out.println("playernumber:7");
+		System.out.println("countrynumber : "+ countrynumber);
+		System.out.println("countryname : "+ countryname);
+		System.out.println("jerscyidnumber : "+ c7.jerscyidnumber);
+		System.out.println("playername : "+ c7.playername);
+		
+		
+		cricket c8 = new cricket();
+		c8.jerscyidnumber= 9 ;
+		c8.playername    ="ravindra jadeja";
+		System.out.println("playernumber:8");
+		System.out.println("countrynumber : "+ countrynumber);
+		System.out.println("countryname : "+ countryname);
+		System.out.println("jerscyidnumber : "+ c8.jerscyidnumber);
+		System.out.println("playername : "+ c8.playername);
+		
+		
+		cricket c9 = new cricket();
+		c9.jerscyidnumber=19;
+		c9.playername    ="umesh";
+		System.out.println("playernumber:9");
+		System.out.println("countrynumber : "+ countrynumber);
+		System.out.println("countryname : "+ countryname);
+		System.out.println("jerscyidnumber : "+ c9.jerscyidnumber);
+		System.out.println("playername : "+ c9.playername);
+		
+		
+		cricket c10 = new cricket();
+		c10.jerscyidnumber=59;
+		c10.playername    ="vijay shankar";
+		System.out.println("playernumber:10");
+		System.out.println("countrynumber : "+ countrynumber);
+		System.out.println("countryname : "+ countryname);
+		System.out.println("jerscyidnumber : "+ c10.jerscyidnumber);
+		System.out.println("playername : "+ c10.playername);
+		
+		
+		
+		cricket c11 = new cricket();
+		c11.jerscyidnumber=30;
+		c11.playername    ="vinay";
+		System.out.println("playernumber:2");
+		System.out.println("countrynumber : "+ countrynumber);
+		System.out.println("countryname : "+ countryname);
+		System.out.println("jerscyidnumber : "+ c11.jerscyidnumber);
+	
+		
+		
+		System.out.println("Thnks for coming to match to play in  t20 world cup in anantapur distict in andhara pradesh state"
+				+ "and other is austial");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}

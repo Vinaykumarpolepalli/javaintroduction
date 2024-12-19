987c345c9093467bb81ad85cd517f7e2db185ccc
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:35:26 +0530

    Add files via upload

---
+++ vinayfirstprogram.java
@@ -0,0 +1,33 @@
+package javademo1;
+
+public class vinayfirstprogram {
+	
+	int number=92;
+	String name="bharat";
+	
+	static int  idno=91;
+	static String countryname="india";
+	
+		
+	//vinayfirstprogram (){
+	//System.out.println();
+	
+     // }
+	
+	//vinayfirstprogram (String = "hai") {
+    //System.out.println("String");
+	//}
+	public static void main(String[] args) {
+		  
+		   System.out.println( idno);
+		   System.out.println( countryname);
+		
+		   
+		//public  void  demo( "thanks");{
+		//System.out.println("namastha");
+		}	
+			
+			
+	
+}
+

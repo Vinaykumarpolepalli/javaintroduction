1e34e0cab27fbf3ef68324efcb7ef709cfca9bec
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:34:50 +0530

    Add files via upload

---
+++ StringReverse1.java
@@ -0,0 +1,24 @@
+package javademo1;
+
+public class StringReverse1 {
+	
+	StringReverse1(String s){
+		System.out.println(s);
+	}
+
+	public static void main(String[] args) {
+		String  s = "namastha vinay";
+		StringReverse1 sr = new StringReverse1(s);
+		//sr.reverse();
+		System.out.println(sr.toString());
+		
+		
+		//
+		//public void reverse();
+			//System.out.println("reverse");
+		//}
+		
+		
+	}
+
+}

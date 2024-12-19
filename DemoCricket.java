24838acc2bc9de1be71121a1070cc33bde3cb2aa
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:32:42 +0530

    Add files via upload

---
+++ DemoCricket.java
@@ -0,0 +1,13 @@
+package javademo1;
+
+public class DemoCricket {
+	
+	
+	public static void main(String args[]) {
+		System.out.println("hi i am main");
+	}
+	static {
+		System.out.println("hi i am in the static block");
+		
+	}
+}

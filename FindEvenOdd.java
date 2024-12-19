bd4a2e7e903b347591c696f73885009960870aa1
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:33:12 +0530

    Add files via upload

---
+++ FindEvenOdd.java
@@ -0,0 +1,25 @@
+package javademo1;
+
+public class FindEvenOdd {
+
+	public static void main(String[] args) {
+		
+		
+		//System.out.println("Enter number :");
+		
+		int i = 567;
+		
+		if (i %2 == 0 ) {
+			System.out.println(  i  + ":is even number " );
+		}
+			
+			else {
+				System.out.println(i +  ":is odd number ");
+				
+			}
+		}
+		
+
+	}
+
+

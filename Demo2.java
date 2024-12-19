259184915a85b9f5e396d843d582350e290e1d4c
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:32:08 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,178 @@
+package javademo1;
+
+public class Demo2 {
+	//instance
+		int jno;
+		String name;
+		//static
+		static int countryid=91;
+		static String countryname="INDIA";
+
+		public static void main(String[] args) {
+			System.out.println("welcome to india players");
+			System.out.println("countryid;"+ countryid);
+			System.out.println("countryname;"+ countryname);
+			
+			
+			
+			System.out.println("all the best for team india");
+			
+			cricket c1 = new cricket();
+	        System.out.println("Player Number: 1");
+	        System.out.println("countryid: " + countryid);
+	        System.out.println("countryname: " + countryname);
+	        System.out.println("jno: " +  c1.jno);
+	        System.out.println("Player Name: " + c1.playername);
+			 
+			 
+			 
+			 cricket c2= new cricket();
+			 c2.jno=45;
+			 c2.playername="rohit sharma";
+			 System.out.println("Playernumber;2");
+			 System.out.println("countryid;"+countryid);
+			 System.out.println("countryname;"+countryname);
+			 System.out.println("jno;"  + c2.jno);
+			 System.out.println("Playername;"+c2.playername);
+			 
+			 
+			 
+			 
+			 cricket c3= new cricket();
+			 c3.jno=18;
+			 c3.playername="virat kohil";
+			 System.out.println("Playernumber;3");
+			 System.out.println("countryid;"+countryid);
+			 System.out.println("countryname;"+countryname);
+			 System.out.println("jno;" +  c3.jno);
+			 System.out.println("Playername;"+c3.playername);
+			 
+			 
+			 
+			 
+			 cricket c4= new cricket();
+			 c4.jno=01;
+			 c4.playername="KL rahul";
+			 System.out.println("Playernumber;4");
+			 System.out.println("countryid;"+countryid);
+			 System.out.println("countryname;"+countryname);
+			 System.out.println("jno;" + c4.jno);
+			 System.out.println("Playername;"+c4.playername);
+			 
+			 
+			 
+			 
+			 cricket c5 = new cricket();
+			 c5.jno=8;
+			 c5.playername="Ravindra Jadeja";
+			 System.out.println("Playernumber;5");
+			 System.out.println("countryid;"+countryid);
+			 System.out.println("countryname;"+countryname);
+			 System.out.println("jno;"+ c5.jno);
+			 System.out.println("Playername;"+c5.playername);
+
+
+
+			 cricket c6 = new cricket();
+			 c6.jno=9;
+			 c6.playername="Sanju Samson";
+			 System.out.println("Playernumber;6");
+			 System.out.println("countryid;"+countryid);
+			 System.out.println("countryname;"+countryname);
+			 System.out.println("jno ;"+   c6.jno);
+			 System.out.println("Playername;"+ c6.playername);
+			 
+			 
+			 
+			 cricket c7 = new cricket();
+			 c7.jno=31;
+			 c7.playername="Ruturaj Gaikwad";
+			 System.out.println("Playernumber;7");
+			 System.out.println("countryid;"+countryid);
+			 System.out.println("countryname;"+countryname);
+			 System.out.println("jno ;"+   c7.jno);
+			 System.out.println("Playername;"+ c7.playername);
+			 
+			 
+			 
+			 
+			 cricket c8 = new cricket();
+			 c8.jno=32;
+			 c8.playername="Ishan Kishan";
+			 System.out.println("Playernumber;8");
+			 System.out.println("countryid;"+countryid);
+			 System.out.println("countryname;"+countryname);
+			 System.out.println("jno;"+   c8.jno);
+			 System.out.println("Playername;"+ c8.playername);
+			 
+			 
+			 
+			 
+			 cricket c9 = new cricket();
+			 c9.jno=35;
+			 c9.playername="Rinku Singh";
+			 System.out.println("Playernumber;9");
+			 System.out.println("countryid;"+countryid);
+			 System.out.println("countryname;"+countryname);
+			 System.out.println("jno;"+   c9.jno);
+			 System.out.println("Playername;"+ c9.playername);
+			 
+			 
+			 
+			 
+			 cricket c10 = new cricket();
+			 c10.jno=48;
+			 c10.playername="Suresh Raina";
+			 System.out.println("Playernumber;10");
+			 System.out.println("countryid;"+countryid);
+			 System.out.println("countryname;"+countryname);
+			 System.out.println("jno;" +   c10.jno);
+			 System.out.println("Playername;"+ c10.playername);
+			 
+			 
+			 
+			 
+			 
+			 cricket c11 = new cricket();
+			 c11.jno=33;
+			 c11.playername="Hardik Pandya";
+			 System.out.println("Playernumber;11");
+			 System.out.println("countryid;"+countryid);
+			 System.out.println("countryname;"+countryname);
+			 System.out.println("jno;"+   c11.jno);
+			 System.out.println("Playername;"+ c11.playername);
+			 
+			 
+			 
+
+			 cricket c12 = new cricket();
+			 c12.jno=4;
+			 c12.playername="T Natarajan";
+			 System.out.println("Playernumber;12");
+			 System.out.println("countryid;"+countryid);
+			 System.out.println("countryname;"+countryname);
+			 System.out.println("jno"+   c12.jno);
+			 System.out.println("Playername;"+ c12.playername);
+			 
+			 
+			 System.out.println("Thanks for coming to play the match and all the best to all players" );
+			 
+			 
+			 
+			
+			
+			
+			
+			
+		}
+
+	}
+			
+			
+			
+			
+
+
+	
+
+

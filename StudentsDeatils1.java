c1ff066d6b25798c0b82115db38eb867d75e42c9
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:35:10 +0530

    Add files via upload

---
+++ StudentsDeatils1.java
@@ -0,0 +1,247 @@
+package javademo1;
+
+public class StudentsDeatils1 {
+	
+	
+	
+	int studentrollnumber = 1 ;
+	String studentname  = "vinay kumar";
+	
+	
+	
+	
+	static String studentbranch = "java full stack";
+	static String studentcollage =  "Vcube";
+	static String studentcurrentcity = "hyd";
+	static int studentsubject = 3;
+	static int studentfees = 30000;
+	static int studentcoursetime = 6;
+	
+	
+	//StudentsDeatils1 (String place){
+	//System.out.println(place);
+	//}
+
+	public static void main(String[] args) {
+		System.out.println("wlecome to IT background it will be more growth in future life ");
+		System.out.println("*********************STUDENT===== 1 ====  *********************************");
+	    StudentsDeatils1 s1 = new StudentsDeatils1 ();
+	    System.out.println("studentrollnumber:"+  s1.studentrollnumber);
+	    System.out.println("studentname:"+ s1.studentname);
+        System.out.println("studentbranch :" + studentbranch);
+        System.out.println("studentcollage :" + studentcollage);
+        System.out.println("studentcurrentcity :" + studentcurrentcity);
+        System.out.println("studentsubject :" + studentsubject);
+        System.out.println("studentfees :" + studentfees);
+        System.out.println("studentcoursetime :" + studentcoursetime);
+        
+        
+        System.out.println("*******************   STUDENT ===2====   ****************************8");
+        StudentsDeatils1 s2 = new StudentsDeatils1 ();
+        s2.studentrollnumber = 2;
+        s2.studentname = "sravan";
+	    System.out.println("studentrollnumber:"+  s2.studentrollnumber);
+	    System.out.println("studentname:"+ s2.studentname);
+        System.out.println("studentbranch :" + studentbranch);
+        System.out.println("studentcollage :" + studentcollage);
+        System.out.println("studentcurrentcity :" + studentcurrentcity);
+        System.out.println("studentsubject :" + studentsubject);
+        System.out.println("studentfees :" + studentfees);
+        System.out.println("studentcoursetime :" + studentcoursetime);
+        
+        System.out.println("*******************   STUDENT ===3====   ****************************8");
+        StudentsDeatils1 s3 = new StudentsDeatils1 ();
+        s3.studentrollnumber = 3;
+        s3.studentname = "vijay";
+	    System.out.println("studentrollnumber:"+  s3.studentrollnumber);
+	    System.out.println("studentname:"+ s3.studentname);
+        System.out.println("studentbranch :" + studentbranch);
+        System.out.println("studentcollage :" + studentcollage);
+        System.out.println("studentcurrentcity :" + studentcurrentcity);
+        System.out.println("studentsubject :" + studentsubject);
+        System.out.println("studentfees :" + studentfees);
+        System.out.println("studentcoursetime :" + studentcoursetime);
+        
+        
+        
+        System.out.println("*******************   STUDENT ===2====   ****************************8");
+        StudentsDeatils1 s4 = new StudentsDeatils1 ();
+        s4.studentrollnumber = 4;
+        s4.studentname = "pavan";
+	    System.out.println("studentrollnumber:"+  s4.studentrollnumber);
+	    System.out.println("studentname:"+ s4.studentname);
+        System.out.println("studentbranch :" + studentbranch);
+        System.out.println("studentcollage :" + studentcollage);
+        System.out.println("studentcurrentcity :" + studentcurrentcity);
+        System.out.println("studentsubject :" + studentsubject);
+        System.out.println("studentfees :" + studentfees);
+        System.out.println("studentcoursetime :" + studentcoursetime);
+        
+        
+        
+        System.out.println("*******************   STUDENT ===2====   ****************************8");
+        StudentsDeatils1 s5 = new StudentsDeatils1 ();
+        s5.studentrollnumber = 5;
+        s5.studentname = "naveen";
+	    System.out.println("studentrollnumber:"+  s2.studentrollnumber);
+	    System.out.println("studentname:"+ s2.studentname);
+        System.out.println("studentbranch :" + studentbranch);
+        System.out.println("studentcollage :" + studentcollage);
+        System.out.println("studentcurrentcity :" + studentcurrentcity);
+        System.out.println("studentsubject :" + studentsubject);
+        System.out.println("studentfees :" + studentfees);
+        System.out.println("studentcoursetime :" + studentcoursetime);
+        
+        
+        
+        
+        System.out.println("*******************   STUDENT ===2====   ****************************8");
+        StudentsDeatils1 s6 = new StudentsDeatils1 ();
+        s6.studentrollnumber = 6;
+        s6.studentname = "vasmshi";
+	    System.out.println("studentrollnumber:"+  s2.studentrollnumber);
+	    System.out.println("studentname:"+ s2.studentname);
+        System.out.println("studentbranch :" + studentbranch);
+        System.out.println("studentcollage :" + studentcollage);
+        System.out.println("studentcurrentcity :" + studentcurrentcity);
+        System.out.println("studentsubject :" + studentsubject);
+        System.out.println("studentfees :" + studentfees);
+        System.out.println("studentcoursetime :" + studentcoursetime);
+        
+        
+        System.out.println("*******************   STUDENT ===7====   ****************************8");
+        StudentsDeatils1 s7 = new StudentsDeatils1 ();
+        s7.studentrollnumber = 7;
+        s7.studentname = "rakesh";
+	    System.out.println("studentrollnumber:"+  s2.studentrollnumber);
+	    System.out.println("studentname:"+ s2.studentname);
+        System.out.println("studentbranch :" + studentbranch);
+        System.out.println("studentcollage :" + studentcollage);
+        System.out.println("studentcurrentcity :" + studentcurrentcity);
+        System.out.println("studentsubject :" + studentsubject);
+        System.out.println("studentfees :" + studentfees);
+        System.out.println("studentcoursetime :" + studentcoursetime);
+        
+        
+        
+        System.out.println("*******************   STUDENT ===8====   ****************************8");
+        StudentsDeatils1 s8 = new StudentsDeatils1 ();
+        s8.studentrollnumber = 8;
+        s8.studentname = "guru";
+	    System.out.println("studentrollnumber:"+  s8.studentrollnumber);
+	    System.out.println("studentname:"+ s8.studentname);
+        System.out.println("studentbranch :" + studentbranch);
+        System.out.println("studentcollage :" + studentcollage);
+        System.out.println("studentcurrentcity :" + studentcurrentcity);
+        System.out.println("studentsubject :" + studentsubject);
+        System.out.println("studentfees :" + studentfees);
+        System.out.println("studentcoursetime :" + studentcoursetime);
+        
+        
+        
+        System.out.println("*******************   STUDENT ===9====   ****************************8");
+        StudentsDeatils1 s9 = new StudentsDeatils1 ();
+        s9.studentrollnumber = 9;
+        s9.studentname = "pranith";
+	    System.out.println("studentrollnumber:"+  s9.studentrollnumber);
+	    System.out.println("studentname:"+ s9.studentname);
+        System.out.println("studentbranch :" + studentbranch);
+        System.out.println("studentcollage :" + studentcollage);
+        System.out.println("studentcurrentcity :" + studentcurrentcity);
+        System.out.println("studentsubject :" + studentsubject);
+        System.out.println("studentfees :" + studentfees);
+        System.out.println("studentcoursetime :" + studentcoursetime);
+        
+        
+        
+        System.out.println("*******************   STUDENT ===10====   ****************************8");
+        StudentsDeatils1 s10 = new StudentsDeatils1 ();
+        s10.studentrollnumber = 10;
+        s10.studentname = "ranjitha";
+	    System.out.println("studentrollnumber:"+  s2.studentrollnumber);
+	    System.out.println("studentname:"+ s2.studentname);
+        System.out.println("studentbranch :" + studentbranch);
+        System.out.println("studentcollage :" + studentcollage);
+        System.out.println("studentcurrentcity :" + studentcurrentcity);
+        System.out.println("studentsubject :" + studentsubject);
+        System.out.println("studentfees :" + studentfees);
+        System.out.println("studentcoursetime :" + studentcoursetime);
+        
+        
+        
+        System.out.println("*******************   STUDENT ===11====   ****************************8");
+        StudentsDeatils1 s11 = new StudentsDeatils1 ();
+        s11.studentrollnumber = 11;
+        s11.studentname = "dileep";
+	    System.out.println("studentrollnumber:"+  s2.studentrollnumber);
+	    System.out.println("studentname:"+ s2.studentname);
+        System.out.println("studentbranch :" + studentbranch);
+        System.out.println("studentcollage :" + studentcollage);
+        System.out.println("studentcurrentcity :" + studentcurrentcity);
+        System.out.println("studentsubject :" + studentsubject);
+        System.out.println("studentfees :" + studentfees);
+        System.out.println("studentcoursetime :" + studentcoursetime);
+        
+        
+        System.out.println("*******************   STUDENT ===112====   ****************************8");
+        StudentsDeatils1 s12 = new StudentsDeatils1 ();
+        s12.studentrollnumber =  12;
+        s12.studentname = "srikantha sir";
+	    System.out.println("studentrollnumber:"+  s2.studentrollnumber);
+	    System.out.println("studentname:"+ s2.studentname);
+        System.out.println("studentbranch :" + studentbranch);
+        System.out.println("studentcollage :" + studentcollage);
+        System.out.println("studentcurrentcity :" + studentcurrentcity);
+        System.out.println("studentsubject :" + studentsubject);
+        System.out.println("studentfees :" + studentfees);
+        System.out.println("studentcoursetime :" + studentcoursetime);
+        
+        
+        System.out.println("*******************   STUDENT ===13====   ****************************8");
+        StudentsDeatils1 s13 = new StudentsDeatils1 ();
+        s13.studentrollnumber = 13;
+        s13.studentname = "ram krishan";
+	    System.out.println("studentrollnumber:"+  s2.studentrollnumber);
+	    System.out.println("studentname:"+ s2.studentname);
+        System.out.println("studentbranch :" + studentbranch);
+        System.out.println("studentcollage :" + studentcollage);
+        System.out.println("studentcurrentcity :" + studentcurrentcity);
+        System.out.println("studentsubject :" + studentsubject);
+        System.out.println("studentfees :" + studentfees);
+        System.out.println("studentcoursetime :" + studentcoursetime);
+        
+        System.out.println("*******************   STUDENT ===14====   ****************************8");
+        StudentsDeatils1 s14 = new StudentsDeatils1 ();
+        s14.studentrollnumber = 14;
+        s14.studentname = "hemanth";
+	    System.out.println("studentrollnumber:"+  s14.studentrollnumber);
+	    System.out.println("studentname:"+ s14.studentname);
+        System.out.println("studentbranch :" + studentbranch);
+        System.out.println("studentcollage :" + studentcollage);
+        System.out.println("studentcurrentcity :" + studentcurrentcity);
+        System.out.println("studentsubject :" + studentsubject);
+        System.out.println("studentfees :" + studentfees);
+        System.out.println("studentcoursetime :" + studentcoursetime);
+        
+        
+        
+        System.out.println("*******************   STUDENT ===15====   ****************************8");
+        StudentsDeatils1 s15 = new StudentsDeatils1 ();
+        s15.studentrollnumber = 15;
+        s15.studentname = "diwakar";
+	    System.out.println("studentrollnumber:"+  s2.studentrollnumber);
+	    System.out.println("studentname:"+ s2.studentname);
+        System.out.println("studentbranch :" + studentbranch);
+        System.out.println("studentcollage :" + studentcollage);
+        System.out.println("studentcurrentcity :" + studentcurrentcity);
+        System.out.println("studentsubject :" + studentsubject);
+        System.out.println("studentfees :" + studentfees);
+        System.out.println("studentcoursetime :" + studentcoursetime);
+        
+        s1.list();
+        
+  }
+	public void list() {
+		System.out.println("total student list in one sheet");
+	}
+}
